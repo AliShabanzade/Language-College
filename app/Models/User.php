7f139dd047a31624bc385496c10bfec8ada6d246
AliShabanzade <shabanzade.ali@gmail.com>
2023-11-04 12:59:33 +0330

    create seeder factory 6 nov 2023

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -23,7 +24,12 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'block',
+        'mobile_number',
         'password',
+        'remember_token',
+
+
     ];
 
     /**
@@ -45,4 +51,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+
+    public function activationCode():HasMany
+    {
+        return $this->hasMany(ActivationCode::class);
+    }
+
 }
