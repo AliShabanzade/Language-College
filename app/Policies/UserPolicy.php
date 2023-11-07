5225dea09547d0f2e3358b1d6f377fbaa6c51d49
AliShabanzade <shabanzade.ali@gmail.com>
2023-11-07 12:56:38 +0330

    user policy added

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Enums\PermissionEnums;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
@@ -12,7 +13,10 @@ class UserPolicy
      */
     public function viewAny(User $user): bool
     {
-        //
+        if ($user->hasAnyPermission(PermissionEnums::USER_ALL->value || PermissionEnums::USER_INDEX->value )) {
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -20,7 +24,10 @@ class UserPolicy
      */
     public function view(User $user, User $model): bool
     {
-        //
+        if ($user->hasAnyPermission(PermissionEnums::USER_ALL->value || PermissionEnums::USER_SHOW->value)) {
+            return true;
+        }
+        return  false;
     }
 
     /**
@@ -28,7 +35,10 @@ class UserPolicy
      */
     public function create(User $user): bool
     {
-        //
+        if ($user->hasAnyPermission(PermissionEnums::USER_ALL->value || PermissionEnums::USER_STORE->value)) {
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -36,7 +46,10 @@ class UserPolicy
      */
     public function update(User $user, User $model): bool
     {
-        //
+        if ($user->hasAnyPermission(PermissionEnums::USER_ALL->value || PermissionEnums::USER_UPDATE->value)) {
+            return true;
+        }
+        return  false;
     }
 
     /**
@@ -44,7 +57,10 @@ class UserPolicy
      */
     public function delete(User $user, User $model): bool
     {
-        //
+        if ($user->hasAnyPermission(PermissionEnums::USER_ALL->value || PermissionEnums::USER_DELETE->value)) {
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -52,7 +68,10 @@ class UserPolicy
      */
     public function restore(User $user, User $model): bool
     {
-        //
+        if ($user->hasAnyPermission(PermissionEnums::USER_RESTORE->value) || PermissionEnums::ADMIN->value) {
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -60,6 +79,9 @@ class UserPolicy
      */
     public function forceDelete(User $user, User $model): bool
     {
-        //
+        if ($user->hasAnyPermission(PermissionEnums::USER_ALL->value) || PermissionEnums::ADMIN->value) {
+            return true;
+        }
+        return false;
     }
 }
