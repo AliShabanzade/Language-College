31d3214b9e357ac0c026a219ac85baadf1e9163a
mahdiyarhosseini9574 <139676228+mahdiyarhosseini9574@users.noreply.github.com>
2023-11-09 16:55:17 +0330

    add register

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @property mixed $mobile_verify_at
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
