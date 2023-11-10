b408f385ccbf7954eac98e258fb1c5d4536010ca
sajad <sajadsoft1@gmail.com>
2023-11-10 17:28:31 +0330

    fix project files

---
+++ app/Console/Commands/MakeRoute.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MakeRoute extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:make-route';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
