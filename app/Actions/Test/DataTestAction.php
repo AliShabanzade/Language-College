3bb886ca7d2454939708077ca9e183f883ca2aa8
sajad <sajadsoft1@gmail.com>
2023-11-10 17:28:31 +0330

    fix project files

---
+++ app/Actions/Test/DataTestAction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions\Test;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class DataTestAction
+{
+    use AsAction;
+
+    public function handle()
+    {
+        // ...
+    }
+}
