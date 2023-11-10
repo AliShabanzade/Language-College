45778cea3e2054774e6737e24cf1b15f59cadc1f
sajad <sajadsoft1@gmail.com>
2023-11-10 17:28:31 +0330

    fix project files

---
+++ app/Actions/Test/UpdateTestAction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions\Test;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class UpdateTestAction
+{
+    use AsAction;
+
+    public function handle()
+    {
+        // ...
+    }
+}
