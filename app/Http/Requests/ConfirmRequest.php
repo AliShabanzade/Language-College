0499475cb61a478743012084a97ab4e107367b78
AliShabanzade <shabanzade.ali@gmail.com>
2023-11-12 12:00:49 +0330

    last change before bot

--- app/Http/Requests/ConfirmRequest.php
+++ app/Http/Requests/ConfirmRequest.php
@@ -14,6 +14,7 @@ class ConfirmRequest extends FormRequest
      */
     public function rules(): array
     {
+
         return [
             'mobile' => 'required|digits:11|regex:/09[0-9]{8}/',
             'code' => 'required|digits:4',
