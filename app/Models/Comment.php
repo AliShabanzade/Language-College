7327ff71e41a39e9a581fbcbf49311635b4a71a4
mahdiyarhosseini9574 <139676228+mahdiyarhosseini9574@users.noreply.github.com>
2023-11-22 11:52:03 +0330

    add comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,10 +2,36 @@
 
 namespace App\Models;
 
+use App\Traits\HasLike;
+use App\Traits\HasSlug;
+use App\Traits\HasUser;
+use App\Traits\HasView;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Comment extends Model
 {
-    use HasFactory;
+    use HasFactory,HasUser,HasView,HasLike,HasSlug;
+
+    protected $fillable = [
+        'user_id', 'parent_id', 'slug', 'commentable_id', 'commentable_type',
+        'title', 'body', 'published'];
+
+
+    public function commentable():MorphTo
+    {
+        return $this->morphTo();
+    }
+    public function child():HasMany
+    {
+        return $this->hasMany(Comment::class,'parent_id');
+    }
+
+    public function getParent():BelongsTo
+    {
+        return $this->belongsTo(Comment::class,'parent_id');
+    }
 }
