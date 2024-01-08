8cf09b270b836161bfb23d15b09310c91d299b2b
AliShabanzade <shabanzade.ali@gmail.com>
2024-01-08 15:18:27 +0330

    pull from zakeri

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -17,8 +17,10 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Image\Manipulations;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 
 class Book extends Model implements HasMedia
